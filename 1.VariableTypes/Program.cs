1f9f87f5190f854f290bc93a34873f95ba5ae5d2
Berat Tastan <beratastan1@gmail.com>
2024-06-01 18:18:21 +0300

    Add project files.

---
+++ 1.VariableTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1.VariableTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname;
+            name = Console.ReadLine();
+            surname = Console.ReadLine();
+            Console.WriteLine($"Hello {name} {surname}");
+        }
+    }
+}

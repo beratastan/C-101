df4aa915edd60eab5195e4e1e6c80c1e8f8d081f
Berat Tastan <beratastan1@gmail.com>
2024-06-02 14:43:03 +0300

    test

---
+++ 2.Operators/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _2.Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Atama ve İşlemli Atama
+            int x = 3;
+            int y = 4;
+
+            y = y + 2;
+            y += 2;
+            y /= 3;
+            x *= 2;
+
+            //Console.WriteLine(y);
+            //Console.WriteLine(x);
+
+            // Mantıksal
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && !isCompleted)
+            {
+                isSuccess = false;
+                isCompleted = true;
+            }
+
+            int a = 3;
+            int b = 4;
+            bool result = a < b;
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
